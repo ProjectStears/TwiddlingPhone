3645a1a6b621814c5bc124e2040b9179ee244ddf
Alexander Scheurer <alexander.scheurer@hs-furtwangen.de>
2016-06-04 02:39:46 +0200

    Serialisation update and basic mechanics

---
+++ Assets/ScoreCounter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+    private Text scoreText;
+    private float score;
+    private GameObject sphere;
+
+    private float maxDistance;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    maxDistance = 5;
+
+        sphere = GameObject.Find("Sphere");
+	    scoreText = GameObject.Find("Score").GetComponent<Text>();
+	    score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    score = score + (maxDistance - Vector3.Magnitude(sphere.transform.position)) * Time.deltaTime * 2;
+	    scoreText.text = Mathf.RoundToInt(score).ToString();
+	}
+}
