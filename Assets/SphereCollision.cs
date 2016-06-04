4ba8af473b397e9f26c287835a633a9178368b65
Alexander Scheurer <mail@aspepex.net>
2016-06-04 20:52:58 +0200

    Hitting the wall now gives feedback, closes #5

---
+++ Assets/SphereCollision.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereCollision : MonoBehaviour
+{
+    private GameObject hitHighlight;
+    private float highlightDuration;
+    private float highlightTimer;
+
+    void Start()
+    {
+        hitHighlight = GameObject.Find("HitHighlight");
+        hitHighlight.SetActive(false);
+
+        highlightDuration = 0.5f;
+        highlightTimer = -1;
+    }
+
+    void Update()
+    {
+        if (highlightTimer > 0)
+        {
+            hitHighlight.SetActive(true);
+            highlightTimer = highlightTimer - Time.deltaTime;
+        }
+        else
+        {
+            hitHighlight.SetActive(false);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Respawn")
+        {
+            highlightTimer = highlightDuration;
+        }
+    }
+}
