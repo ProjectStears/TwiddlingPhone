e8a0bedf63b51d489289c871ba89ca28c65cf502
Alexander Scheurer <mail@aspepex.net>
2016-06-06 16:23:38 +0200

    Showing an alert on touch for presentation purposes. Will later be remote controlled. closes #8

---
+++ Assets/Scripts/Alerter.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alerter : MonoBehaviour
+{
+    private GameObject alertPanel;
+    private bool lastState;
+    private float alertTimer;
+    private float alertDuration;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    alertDuration = 5;
+	    alertTimer = -1;
+
+	    alertPanel = GameObject.Find("AlertPanel");
+        alertPanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    if (alertTimer > 0)
+	    {
+	        alertTimer = alertTimer - Time.deltaTime;
+	    }
+	    else
+	    {
+	        alertPanel.SetActive(false);
+	    }
+
+
+	    if (Input.touchCount > 0)
+	    {
+	        if (!lastState && alertTimer < 0)
+	        {
+	            ShowAlert();
+	        }
+
+	        lastState = true;
+	    }
+	    else
+	    {
+	        lastState = false;
+	    }
+	}
+
+    private void ShowAlert()
+    {
+        alertTimer = alertDuration;
+        alertPanel.SetActive(true);
+        Handheld.Vibrate();
+    }
+}
